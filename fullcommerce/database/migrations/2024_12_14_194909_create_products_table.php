d696dcb11e210da215a9520579b34a33238de377
FerrazRezende <mtsferraz.rezende@gmail.com>
2024-12-14 18:11:12 -0300

    add products table

--- fullcommerce/database/migrations/2024_12_14_194909_create_products_table.php
+++ fullcommerce/database/migrations/2024_12_14_194909_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enum\ProductCategory;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,10 +13,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->uuid();
             $table->string('name', 255);
             $table->text('description');
             $table->float('price');
+            $table->integer('amount');
+            $table->enum('category', ProductCategory::values());
             $table->timestamps();
         });
     }
